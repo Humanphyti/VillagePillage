b3bb7d4c9e896e55c951a352780d57bfeda83915
Humanphyti <humanphyti@gmail.com>
2019-09-16 23:35:07 -0700

    added people and inheritance classes
    created the base village people classes that still need some testing and are curently untested.

---
+++ Assets/Scripts/ActorScripts/Jarl.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Jarl : People
+{
+    //private float leadership;
+    protected Jarl(int strength, bool sex, int age, int wealth, float leadership, float greed, ArrayList talkingLines, Dictionary<string, string> villageInfo)
+    {
+        this.strength = strength;
+        this.sex = sex;
+        this.age = age;
+        this.wealth = wealth;
+        this.leadership = leadership;
+        this.greed = greed;
+        this.talkingLines = talkingLines;
+        this.villageInfo = villageInfo;
+    }
+
+    protected void setGreed(float greed)
+    {
+        this.greed = greed;
+    }
+
+    protected override float getGreed()
+    {
+        return greed;
+    }
+
+    protected override int getIntelligence()
+    {
+        return 0;
+    }
+
+    protected void setLeadership(float leadership)
+    {
+        
+    }
+
+    protected override float getLeadership()
+    {
+        return leadership;
+    }
+
+    protected override int getTruthChance()
+    {
+        return 0;
+    }
+
+    protected override bool getTruthCheck()
+    {
+        return false;
+    }
+
+    protected override ArrayList getWeaponProficiencies()
+    {
+        return null;
+    }
+
+    protected override ArrayList getTalkingLines()
+    {
+        return talkingLines;
+    }
+
+    protected override int getWeaponSkill()
+    {
+        return 0;
+    }
+
+    protected override Dictionary<string, string> getVillageInfo()
+    {
+        return villageInfo;
+    }
+}
